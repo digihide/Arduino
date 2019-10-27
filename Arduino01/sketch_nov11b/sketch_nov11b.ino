3746c624ebd88e7beb3827f7d14387e6e03214b7
digihide <digihide@gmail.com>
2019-10-27 11:37:15 +0900

    Arduino for MIDI

---
+++ Arduino01/sketch_nov11b/sketch_nov11b.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9800);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  Serial.println("Hello, World");
+  delay(1000);
+
+}
